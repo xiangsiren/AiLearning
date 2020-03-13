ecb52b726e119bb5d82f1bf809cbb04701b355dc
xiangsiren <981620741@qq.com>
2020-03-13 12:11:10 +0800

    创建学习

--- ml/ren.py
+++ ml/ren.py
@@ -4,6 +4,18 @@
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+import numpy as np
 # 安装 TensorFlow
-
 import tensorflow as tf
+
+from matplotlib import *
+from matplotlib import pyplot as plt
+
+x = np.arange(1, 11)
+y = 2 * x + 5
+print(x)
+plt.title("Matplotlib demo")
+plt.xlabel("x axis caption")
+plt.ylabel("y axis caption")
+plt.plot(x, y)
+plt.show()
