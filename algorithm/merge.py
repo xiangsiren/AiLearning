6d09404c749e1b17a2407628fd561bdf3b89e996
xiangsiren <981620741@qq.com>
2020-03-16 20:41:53 +0800

    创建学习

---
+++ algorithm/merge.py
@@ -0,0 +1,27 @@
+def mergeSort(arr):
+    import math
+    if(len(arr)<2):
+        return arr
+    middle = math.floor(len(arr)/2)
+    left, right = arr[0:middle], arr[middle:]
+    return merge(mergeSort(left), mergeSort(right))
+
+def merge(left,right):
+    result = []
+    while left and right:
+        if left[0] <= right[0]:
+            result.append(left.pop(0))
+        else:
+            result.append(right.pop(0));
+    while left:
+        result.append(left.pop(0))
+    while right:
+        result.append(right.pop(0));
+    return result
+
+
+if __name__ == '__main__':
+
+    arr = [23,33,2,4,5,77,24,3,9]
+
+    print(mergeSort(arr))
