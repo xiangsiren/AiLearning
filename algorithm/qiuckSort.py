3dae3cd7a9d615a2c34124448c65a346a6b213a2
xiangsiren <981620741@qq.com>
2020-03-22 10:23:10 +0800

    创建学习

---
+++ algorithm/qiuckSort.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def qiuksort(arr):
+
+    left ,right = 0, len(arr)- 1
+
+
+
+
+    print(arr)
+
+
+if __name__ == "__main__":
+
+    arr = [23, 33, 2, 4, 5, 4, 3, 77, 24, 3, 9]
+
+    print(qiuksort(arr))
+
---
+++ algorithm/quik.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+快速排序
+"""
+
+
+def quickSort(arr, left=None, right=None):
+    left = 0 if not isinstance(left,(int, float)) else left
+    right = len(arr)-1 if not isinstance(right,(int, float)) else right
+    if left < right:
+        partitionIndex = partition(arr, left, right)
+        quickSort(arr, left, partitionIndex-1)
+        quickSort(arr, partitionIndex+1, right)
+    return arr
+
+def partition(arr, left, right):
+    pivot = left
+    index = pivot+1
+    i = index
+    while(i <= right):
+        if arr[i] < arr[pivot]:
+            swap(arr, i, index)
+            index+=1
+        i+=1
+    swap(arr,pivot,index-1)
+    return index-1
+
+def swap(arr, i, j):
+    arr[i], arr[j] = arr[j], arr[i]
+
+
+if __name__ == "__main__":
+
+    arr = [23, 33, 2, 4, 5, 4, 3, 77, 24, 3, 9]
+
+    print(quickSort(arr))
